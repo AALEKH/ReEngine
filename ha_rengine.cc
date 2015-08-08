c13c2b1b2c89d751e614475abd0de1935c2ce525
aalekh <aalekh1993@rediffmail.com>
2015-08-08 15:11:39 +0530

    Added Enviornment variable support, some MsgPack + Redis Function and MsgPack Library

--- ha_rengine.cc
+++ ha_rengine.cc
@@ -1,23 +1,25 @@
+
+// For Redis and basic C++ operations
 #include <iostream>
-#include <queue>
-#include <thread>
-#include <assert.h>
+#include <string>
+#include <vector>
+#include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <msgpack.hpp>
+#include "hiredis/hiredis.h"
+#include "hiredis/async.h"
+#include "hiredis/adapters/libevent.h"
+
+// SQL Plugin header's
 #include "sql_class.h"           // MYSQL_HANDLERTON_INTERFACE_VERSION
-#include "ha_rengine.h"
-#include "probes_mysql.h"
+#include "ha_example.h"
 #include "sql_plugin.h"
+#include "probes_mysql.h"
 
-// For Redis Cluster Operations
-
-#include "createcluster.h"
-#include "cpp-hiredis-cluster/include/hirediscommand.h"
-
-using namespace RedisCluster;
-using std::string;
-using std::cout;
-using std::cerr;
-using std::endl;
+// using namespace RedisCluster;
 using namespace std;
+using namespace msgpack;
 
 
 static handler *rengine_create_handler(handlerton *hton, TABLE_SHARE *table, MEM_ROOT *mem_root);
@@ -117,32 +119,45 @@ static bool rengine_is_supported_system_table(const char *db,
   return false;
 }
 
-// For pushing data into redis cluster
-void processClusterCommand(const char *address, int port)
-{
-    Cluster<redisContext>::ptr_t cluster_p;
-    redisReply * reply;
-    int value = 0;
-    string command;
-    cluster_p = HiredisCommand<>::createCluster( address, port );
-    cout << "Bus error 0" << endl;
-    while (value < 20) {
-      command = "FOO";
-      cout << "Bus Error 1" << endl;
-      command = command + to_string(value);
-      cout << "Bus Error 2" << endl;
-      reply = static_cast<redisReply*>( HiredisCommand<>::Command( cluster_p, "FOO", "SET %s %i", command.c_str(), value));
+//// For Redis Function
 
-      if( reply->type == REDIS_REPLY_STATUS  || reply->type == REDIS_REPLY_ERROR )
-      {
-          cout << " Reply to SET FOO BAR " << endl;
-          cout << reply->str << endl;
-      }
-      value = value + 1;
-      freeReplyObject( reply );
-    }
-    delete cluster_p;
+int pushAsMsgPack(vector<int> data, char *key){
+  redisContext *c;
+  redisReply *reply;
+  c = redisConnect("127.0.0.1", 6379);
+  if(c!=NULL && c->err) {
+      printf("Error: %s\n", c->errstr);
+  }
+
+  sbuffer sbuf;
+  pack(sbuf, data);
+
+  reply = (redisReply *)redisCommand(c,"SET %s %s", key, sbuf.data());
+  freeReplyObject(reply);
+  return 0;
 }
+
+object getAsMsgPack(char *key) {
+
+  redisContext *c;
+  redisReply *reply;
+  int sizeMSG = 6028;
+  c = redisConnect("127.0.0.1", 6379);
+  if(c!=NULL && c->err) {
+      printf("Error: %s\n", c->errstr);
+  }
+  reply = (redisReply *)redisCommand(c, "GET %s", key);
+  char* sbuf2 = reply->str;
+  unpacked msg;
+  unpack(&msg, sbuf2, sizeMSG);
+  object obj = msg.get();
+  freeReplyObject(reply);
+  redisFree(c);
+  return obj;
+}
+
+
+//// End of Redis Function
 
 int ha_rengine::open(const char *name, int mode, uint test_if_locked)
 {
@@ -350,9 +365,7 @@ int ha_rengine::create(const char *name, TABLE *table_arg,
 struct st_mysql_storage_engine rengine_storage_engine=
 { MYSQL_HANDLERTON_INTERFACE_VERSION };
 
-static ulong srv_enum_var= 0;
-static ulong srv_ulong_var= 0;
-static double srv_double_var= 0;
+static uint expiry_time_value = 1000;
 
 const char *enum_var_names[]=
 {
@@ -365,105 +378,44 @@ TYPELIB enum_var_typelib=
   enum_var_names, NULL
 };
 
-static MYSQL_SYSVAR_ENUM(
-  enum_var,                       // name
-  srv_enum_var,                   // varname
-  PLUGIN_VAR_RQCMDARG,            // opt
-  "Sample ENUM system variable.", // comment
-  NULL,                           // check
-  NULL,                           // update
-  0,                              // def
-  &enum_var_typelib);             // typelib
+static MYSQL_THDVAR_UINT(
+  expiry_time,
+  expiry_time,
+  PLUGIN_VAR_OPCMDARG,
+  "Sets Expiry Time for Redis Key Pair",
+  NULL,
+  NULL,
+  expiry_time_value,
+  1,
+  10000,
+  10
+);
 
-static MYSQL_SYSVAR_ULONG(
-  ulong_var,
-  srv_ulong_var,
-  PLUGIN_VAR_RQCMDARG,
-  "0..1000",
-  NULL,
-  NULL,
-  8,
-  0,
-  1000,
-  0);
-
-static MYSQL_SYSVAR_DOUBLE(
-  double_var,
-  srv_double_var,
-  PLUGIN_VAR_RQCMDARG,
-  "0.500000..1000.500000",
-  NULL,
-  NULL,
-  8.5,
-  0.5,
-  1000.5,
-  0);                             // reserved always 0
-
-static MYSQL_THDVAR_DOUBLE(
-  double_thdvar,
-  PLUGIN_VAR_RQCMDARG,
-  "0.500000..1000.500000",
-  NULL,
-  NULL,
-  8.5,
-  0.5,
-  1000.5,
-  0);
-
+// Null Termination is important as per convention, this structure adds the variable to POSIX thread
 static struct st_mysql_sys_var* rengine_system_variables[]= {
-  MYSQL_SYSVAR(enum_var),
-  MYSQL_SYSVAR(ulong_var),
-  MYSQL_SYSVAR(double_var),
-  MYSQL_SYSVAR(double_thdvar),
+  MYSQL_SYSVAR(expiry_time),
   NULL
 };
 
-static int show_func_rengine(MYSQL_THD thd, struct st_mysql_show_var *var,
-                             char *buf)
+static int show_func_rengine(MYSQL_THD thd, struct st_mysql_show_var *var, char *buf)
 {
   var->type= SHOW_CHAR;
   var->value= buf; // it's of SHOW_VAR_FUNC_BUFF_SIZE bytes
-  my_snprintf(buf, SHOW_VAR_FUNC_BUFF_SIZE,
-              "enum_var is %lu, ulong_var is %lu, "
-              "double_var is %f, %.6b", // %b is a MySQL extension
-              srv_enum_var, srv_ulong_var, srv_double_var, "really");
+  my_snprintf(buf, SHOW_VAR_FUNC_BUFF_SIZE, "Expiry Time is : %i", expiry_time_value);
   return 0;
 }
 
 struct rengine_vars_t
 {
-	ulong  var1;
-	double var2;
-	char   var3[64];
-  bool   var4;
-  bool   var5;
-  ulong  var6;
+	int  var1;
 };
 
-rengine_vars_t rengine_vars= {100, 20.01, "three hundred", true, 0, 8250};
-
+// rengine_vars_t rengine_vars= {100, 20.01, "three hundred", true, 0, 8250};
+rengine_vars_t rengine_vars = {1000};
 static st_mysql_show_var show_status_rengine[]=
 {
-  {"var1", (char *)&rengine_vars.var1, SHOW_LONG, SHOW_SCOPE_GLOBAL},
-  {"var2", (char *)&rengine_vars.var2, SHOW_DOUBLE, SHOW_SCOPE_GLOBAL},
+  {"expiry_time", (char *)&rengine_vars.var1, SHOW_INT, SHOW_SCOPE_GLOBAL},
   {0,0,SHOW_UNDEF, SHOW_SCOPE_UNDEF} // null terminator required
-};
-
-static struct st_mysql_show_var show_array_rengine[]=
-{
-  {"array", (char *)show_status_rengine, SHOW_ARRAY, SHOW_SCOPE_GLOBAL},
-  {"var3", (char *)&rengine_vars.var3, SHOW_CHAR, SHOW_SCOPE_GLOBAL},
-  {"var4", (char *)&rengine_vars.var4, SHOW_BOOL, SHOW_SCOPE_GLOBAL},
-  {0,0,SHOW_UNDEF, SHOW_SCOPE_UNDEF}
-};
-
-static struct st_mysql_show_var func_status[]=
-{
-  {"rengine_func_rengine", (char *)show_func_rengine, SHOW_FUNC, SHOW_SCOPE_GLOBAL},
-  {"rengine_status_var5", (char *)&rengine_vars.var5, SHOW_BOOL, SHOW_SCOPE_GLOBAL},
-  {"rengine_status_var6", (char *)&rengine_vars.var6, SHOW_LONG, SHOW_SCOPE_GLOBAL},
-  {"rengine_status",  (char *)show_array_rengine, SHOW_ARRAY, SHOW_SCOPE_GLOBAL},
-  {0,0,SHOW_UNDEF, SHOW_SCOPE_UNDEF}
 };
 
 mysql_declare_plugin(rengine)
@@ -476,8 +428,8 @@ mysql_declare_plugin(rengine)
   PLUGIN_LICENSE_GPL,
   rengine_init_func,                            /* Plugin Init */
   NULL,                                         /* Plugin Deinit */
-  0x0001 /* 0.1 */,
-  func_status,                                  /* status variables */
+  0x0001,                                       /* 0.1 */
+  show_status_rengine,                          /* status variables */
   rengine_system_variables,                     /* system variables */
   NULL,                                         /* config options */
   0,                                            /* flags */
